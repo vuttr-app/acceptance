cf1e639412b6ead0089a7605f17c55c52dd9ed23
mestihudson <mestihudson@gmail.com>
2019-04-14 23:53:34 -0300

    Adicionada verificação de quantidade de elementos
      Agora verifica, de fato, se a quantidade correta é apresentada.

--- step-definitions/listar-todas-as-ferramentas.steps.js
+++ step-definitions/listar-todas-as-ferramentas.steps.js
@@ -1,10 +1,13 @@
 import { Given, When, Then } from 'cucumber'
 import { client } from 'nightwatch-api'
+import { expect } from 'chai'
 import fs from 'fs'
 
 Given('que existe uma ferramenta', async () => {
-  const content = JSON.stringify({ tools: [{ "title": "Ferramenta 1" }] })
-  await fs.writeFile('/ws/api/db.json', content, (error) => {
+  const content = JSON.stringify({ tools: [
+    { title: 'Ferramenta 1' }
+  ]})
+  await fs.writeFile('/ws/api/.db.json', content, (error) => {
     if (error) {
       return console.error(error)
     }
@@ -19,5 +22,7 @@ When('eu solicito a lista de todas as ferramentas', async () => {
 
 Then('eu verifico que a lista apresentada possui uma ferramenta', async () => {
   await client
-    .waitForElementPresent(`//*[@data-set='ferramenta']`)
+    .elements('xpath', `//*[@data-set='ferramenta']`, (result) => {
+      expect(result.value.length).to.be.equals(1)
+    })
 })
