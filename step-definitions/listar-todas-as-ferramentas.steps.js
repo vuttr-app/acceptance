e9688553874c37616860d16726d8341eca1c0d4e
mestihudson <mestihudson@gmail.com>
2019-04-14 23:20:15 -0300

    Refatoração: substitui const pode env var

--- step-definitions/listar-todas-as-ferramentas.steps.js
+++ step-definitions/listar-todas-as-ferramentas.steps.js
@@ -4,17 +4,15 @@ import fs from 'fs'
 
 Given('que existe uma ferramenta', async () => {
   const content = JSON.stringify({ tools: [{ "title": "Ferramenta 1" }] })
-  console.log(content)
   await fs.writeFile('/ws/api/db.json', content, (error) => {
     if (error) {
       return console.error(error)
     }
-    console.log()
   })
 })
 
 When('eu solicito a lista de todas as ferramentas', async () => {
-  const url = 'http://172.21.1.3:4200'
+  const url = process.env.APP_URL
   await client
     .url(url)
 })
